b622a49990ce752d8906966ff6a6ca44d021c5c0
jahu <jan.huehne@weimar.eins.de>
2009-05-16 03:57:19 -0700

    user assoziation.

--- lib/rating.rb
+++ lib/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ActiveRecord::Base
   belongs_to :rate
   belongs_to :rateable, :polymorphic => true
-  
+  belongs_to :user
+
   validates_uniqueness_of :user_id, :scope => [:rateable_id, :rateable_type]
 end
