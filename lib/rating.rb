14ca666c209abe38d574a2c678e63b72e2a6dcc0
Jan Hühne <jahu@noname.(none)>
2009-05-29 11:29:43 +0200

    named_scope um Ratings zu finden die nicht von bestimmten Usern sind.

--- lib/rating.rb
+++ lib/rating.rb
@@ -4,4 +4,7 @@ class Rating < ActiveRecord::Base
   belongs_to :user
 
   validates_uniqueness_of :user_id, :scope => [:rateable_id, :rateable_type]
+  
+  named_scope :without_user, lambda { |*user| { :conditions => [ 'user_id NOT IN (?)', user.collect(&:id) ] } }
+  
 end
